bed03da7f991fb050496fe620a51a48d5e948e4d
Mario Pinto <mario@leanmind.es>
2023-02-05 23:41:27 +0000

    🔴 Password validator should evaluate as invalid a password that has not at least a number

--- src/tests/password_validator.test.ts
+++ src/tests/password_validator.test.ts
@@ -28,4 +28,8 @@ describe('Password validator should', () => {
 	it('evaluate as invalid a password that has not at least a lowercase letter', () => {
 		expect(is_valid_password('A23N_E')).toBeFalsy();
 	});
+
+	it('evaluate as invalid a password that has not at least a number', () => {
+		expect(is_valid_password('aaan_E')).toBeFalsy();
+	});
 });
