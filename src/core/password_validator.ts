e0bbee201176f8be1bffee20fbcb48c6246b3376
Mario Pinto <mario@leanmind.es>
2023-02-06 01:05:46 +0000

    🔵 Inline and inversion of conditionals to simplify code and gain performance avoiding more validations than is necessary

--- src/core/password_validator.ts
+++ src/core/password_validator.ts
@@ -1,28 +1,25 @@
 function HasMinimumLength(password: string, minimum_length: number) {
-	return password.length < minimum_length;
+	return password.length >= minimum_length;
 }
 
 function HasLowercaseLetter(password: string) {
-	return password.toLowerCase() === password;
+	return password.toLowerCase() !== password;
 }
 
 function HasUppercaseLetter(password: string) {
-	return password.toUpperCase() === password;
+	return password.toUpperCase() !== password;
 }
 
 function ThereIsANumber(password: string) {
-	return password.search('[0-9]') === -1;
+	return password.search('[0-9]') !== -1;
 }
 
 export function is_valid_password(password: string): boolean {
 	const minimum_length = 6;
-	const has_minimum_length = HasMinimumLength(password, minimum_length);
-	const has_lowercase_letter = HasLowercaseLetter(password);
-	const has_uppercase_letter = HasUppercaseLetter(password);
-	const there_is_a_number = ThereIsANumber(password);
-
-	return !there_is_a_number &&
-			!has_minimum_length &&
-			!has_lowercase_letter &&
-			!has_uppercase_letter;
+	return (
+		ThereIsANumber(password) &&
+		HasMinimumLength(password, minimum_length) &&
+		HasLowercaseLetter(password) &&
+		HasUppercaseLetter(password)
+	);
 }
